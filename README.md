b3db9c2771f0dc4b9518aca22ee40976d05c0e61
Edwin <edgoca2498@gmail.com>
2024-02-21 10:27:31 -0600

    :sparkles: Add schema validation and update readme.md

--- README.md
+++ README.md
@@ -5,19 +5,32 @@
 # Ejecutar en desarrollo
 
 1. Clonar el repositorio
+
 2. Ejecutar
 ```
 yarn install
 ```
+
 3. Tener Nest CLI instalado
 ```
 npm i -g @nestjs/cli
 ```
+
 4. Levantar la base de datos
 ```
 docker-compose up -d
 ```
-5. Reconstruir la base de datos usando un seed
+
+5. Clonar el archivo __.env.example__ y renombrar la copia a __.env__
+
+6. Llenar las variables de entorno definidas en el __.env__
+
+7. Ejecutar la aplicación en dev:
+```
+yarn start:dev
+```
+
+8. Reconstruir la base de datos usando un seed
 ```
 http://localhost:3000/api/v2/seed
 ```
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -4,6 +4,7 @@ import {
   InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Model, isValidObjectId } from 'mongoose';
@@ -16,6 +17,7 @@ export class PokemonService {
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly configService: ConfigService,
   ) {}
 
   async create(createPokemonDto: CreatePokemonDto) {
@@ -28,7 +30,8 @@ export class PokemonService {
   }
 
   async findAll(paginationDto: PaginationDto) {
-    const { limit = 10, offset = 0 } = paginationDto;
+    const { limit = this.configService.get('defaultLimit'), offset = 0 } =
+      paginationDto;
     return await this.pokemonModel
       .find()
       .limit(limit)
